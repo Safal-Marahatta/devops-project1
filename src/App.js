14104f817eb2f74bbd5a3d0cd72da0c2d7e3bab8
Safal-Marahatta <safalmarahatta123456@gmail.com>
2024-07-16 16:48:25 +0545

    last

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://54.254.188.30:3001/api/submit', {
+      const response = await fetch('http://54.255.113.106:3001/api/submit', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
