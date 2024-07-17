126b2a7b7180a278f31cde423c9dc07c1e56621b
Safal-Marahatta <safalmarahatta123456@gmail.com>
2024-07-17 16:56:33 +0545

    3000:3000

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://api_safal.learn.cloudlaya.com:5000/api/submit', {
+      const response = await fetch('http://api_safal.learn.cloudlaya.com/api/submit', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
